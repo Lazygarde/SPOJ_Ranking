452c14fe25d383fa34b66c7d8dddebe917b426ef
Đỗ Hướng <dohuong0422@gmail.com>
2023-02-22 08:04:29 +0700

    coroutine

--- app/src/main/java/com/example/spojranking/data/GetWebData.kt
+++ app/src/main/java/com/example/spojranking/data/GetWebData.kt
@@ -1,5 +1,9 @@
 package com.example.spojranking.data
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
@@ -7,47 +11,44 @@ import java.io.IOException
 import java.lang.String
 import kotlin.Int
 
-
 class GetWebData {
-    fun get(): MutableList<User> {
-        val list = mutableListOf<User>()
+    suspend fun get(): List<User> = withContext(Dispatchers.IO) {
         val users = getListOfUser()
-        val src = "https://www.spoj.com/PTIT/users/"
-        for (i in 0 until users.size) {
-            try {
-                val url = src + users[i].userName + "/"
-                var cnt = 0
-                val doc = Jsoup.connect(url).get()
-                val table: Element? = doc.select("table.table-condensed").first()
-                if (table != null) {
-                    val listData: Elements = table.select("td > a")
-                    if (listData.size > 0) {
-                        for (data in listData) {
-                            if (data.attr("href")
-                                    .contains(users[i].userName) && data.attr("href")
-                                    .contains("/PTIT/status/")
-                            ) {
-                                val index1 = String.valueOf(data).indexOf("/PTIT/status/")
-                                val index2: Int =
-                                    String.valueOf(data).indexOf(users[i].userName)
-                                if (String.valueOf(data)
-                                        .substring(index1, index2).length > 14
+        val deferredUsers = users.map { user ->
+            async {
+                try {
+                    val url = "https://www.spoj.com/PTIT/users/${user.userName}/"
+                    val doc = Jsoup.connect(url).get()
+                    val table: Element? = doc.select("table.table-condensed").first()
+                    var cnt = 0
+                    if (table != null) {
+                        val listData: Elements = table.select("td > a")
+                        if (listData.size > 0) {
+                            for (data in listData) {
+                                if (data.attr("href")
+                                        .contains(user.userName) && data.attr("href")
+                                        .contains("/PTIT/status/")
                                 ) {
-                                    cnt++
+                                    val index1 = String.valueOf(data).indexOf("/PTIT/status/")
+                                    val index2: Int =
+                                        String.valueOf(data).indexOf(user.userName)
+                                    if (String.valueOf(data)
+                                            .substring(index1, index2).length > 14
+                                    ) {
+                                        cnt++
+                                    }
                                 }
                             }
                         }
-                        users[i].setSolve(cnt)
-                        list.add(users[i])
                     }
+                    user.setSolve(cnt)
+                    user
+                } catch (e: IOException) {
+                    user
                 }
-
-            } catch (e: IOException) {
-                continue
             }
         }
-        list.sortBy { -it.solved }
-        return list
+        val result = deferredUsers.awaitAll()
+        result.sortedByDescending { it.solved }
     }
-
 }
